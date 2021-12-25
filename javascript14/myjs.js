f0fe19ab948a585889e11ac5a5fe3304f0ba4a32
範雷雷 <95039519+leileifan166@users.noreply.github.com>
2021-12-25 15:28:01 +0900

    Add files via upload

---
+++ javascript14/myjs.js
@@ -0,0 +1,9 @@
+let number2 = 2020;
+let number3 = 0.049;
+document.write(number2 + '年房贷利率为: ' + number3);
+document.write("<br>")
+let number4 = 43.75;
+document.write('该顾客购买猪肉的价格为: ' + number4 + '元<br>');
+
+
+
